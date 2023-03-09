ca5602b8d75f202e9b6f00ec3645067b51e9f522
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-03-09 22:35:30 +0000

    Checking for linter errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 
-import addUserScores from './modules/addUserScores.js'
-import refresh from './modules/refresh.js'
-addUserScores()
-refresh()
+import addUserScores from './modules/addUserScores.js';
+import refresh from './modules/refresh.js';
+
+addUserScores();
+refresh();
--- src/modules/addUserScores.js
+++ src/modules/addUserScores.js
@@ -1,22 +1,21 @@
-import Leaderboard from "./Leaderboard.js";
+import Leaderboard from './Leaderboard.js';
 
 const game = new Leaderboard('aOURfqS6nBnP825EHCwx', 'https://us-central1-js-capstone-backend.cloudfunctions.net/api');
 
 const addUserScores = () => {
-    const form = document.getElementById('form');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const name = document.getElementById('name').value.trim();
-        const score = document.getElementById('score').value.trim();
-        if (!name || !score) {
-            return;
-        }
+  const form = document.getElementById('form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('name').value.trim();
+    const score = document.getElementById('score').value.trim();
+    if (!name || !score) {
+      return;
+    }
 
-        game.addScore(name, score);
-        game.refreshScores();
-        form.reset();
+    game.addScore(name, score);
+    game.refreshScores();
+    form.reset();
+  });
+};
 
-    });
-}
-
-export default addUserScores
+export default addUserScores;
