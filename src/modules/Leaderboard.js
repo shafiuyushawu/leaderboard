c270f72e2f19980f0d996646ba0a3c5facf6a33f
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-03-10 07:22:39 +0000

    Final UI touches

--- src/modules/Leaderboard.js
+++ src/modules/Leaderboard.js
@@ -1,54 +1,55 @@
 class Leaderboard {
-  constructor(gameId, apiEndpoint) {
-    this.gameId = gameId;
-    this.apiEndpoint = apiEndpoint;
-    this.ul = document.getElementById('ul');
-  }
-
-  async getScores() {
-    try {
-      const response = await fetch(`${this.apiEndpoint}/games/${this.gameId}/scores`);
-      const data = await response.json();
-      return data.result;
-    } catch (error) {
-      error(`Failed to fetch scores: ${error}`);
-      return [];
+    constructor(gameId, apiEndpoint) {
+        this.gameId = gameId;
+        this.apiEndpoint = apiEndpoint;
+        this.ul = document.getElementById('ul');
     }
-  }
 
-  async addScore(name, score) {
-    try {
-      const response = await fetch(`${this.apiEndpoint}/games/${this.gameId}/scores`, {
-        method: 'POST',
-        headers: {
-          'content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          user: name,
-          score,
-        }),
-      });
-      const data = await response.json();
-      return data.result;
-    } catch (error) {
-      error(`Failed to add score: ${error}`);
-      return null;
+    async getScores() {
+        try {
+            const response = await fetch(`${this.apiEndpoint}/games/${this.gameId}/scores`);
+            const data = await response.json();
+            return data.result;
+        } catch (error) {
+            error(`Failed to fetch scores: ${error}`);
+            return [];
+        }
     }
-  }
 
-  displayScores(scores) {
-    this.ul.innerHTML = '';
-    scores.forEach((score) => {
-      const li = document.createElement('li');
-      li.textContent = `Name: ${score.user} Score: ${score.score}`;
-      this.ul.appendChild(li);
-    });
-  }
+    async addScore(name, score) {
+        try {
+            const response = await fetch(`${this.apiEndpoint}/games/${this.gameId}/scores`, {
+                method: 'POST',
+                headers: {
+                    'content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user: name,
+                    score,
+                }),
+            });
+            const data = await response.json();
+            return data.result;
+        } catch (error) {
+            error(`Failed to add score: ${error}`);
+            return null;
+        }
+    }
 
-  async refreshScores() {
-    const scores = await this.getScores();
-    this.displayScores(scores);
-  }
+    displayScores(scores) {
+        this.ul.innerHTML = '';
+        scores.forEach((score) => {
+            const li = document.createElement('li');
+            li.className = 'px-2 py-1 text-2xl text-white even:bg-slate-200 even:text-black'
+            li.textContent = `Name: ${score.user} Score: ${score.score}`;
+            this.ul.appendChild(li);
+        });
+    }
+
+    async refreshScores() {
+        const scores = await this.getScores();
+        this.displayScores(scores);
+    }
 }
 
 export default Leaderboard;
